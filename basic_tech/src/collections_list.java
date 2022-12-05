cf6c048fb966a1839c5de7864a155b44384f1382
karthiga_k <karthiga_k@LT2574.solartistech.local>
2022-12-05 15:56:18 +0530

    change in collections_list

--- basic_tech/src/collections_list.java
+++ basic_tech/src/collections_list.java
@@ -4,11 +4,11 @@ public class collections_list {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> list= new ArrayList<Integer>();
-		list.add(9);
-		list.add(5);
-		list.add(8);
-		list.add(4);
 		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
 		
 //		Iterator itr=list.iterator();
 //		while(itr.hasNext()) {
